fd68b22073aa544d3100ac86ba7bf06dd95b8809
myzen2 <mehdi.zenbil@orange.fr>
2016-02-23 09:16:37 +0100

    changing logo

--- app/models/utilisateur.rb
+++ app/models/utilisateur.rb
@@ -2,5 +2,5 @@ class Utilisateur < ActiveRecord::Base
   belongs_to :user
   has_many :articles
 
-  default_scope :order => 'articles.created_at DESC'
+  #default_scope :order => 'articles.created_at DESC'
 end
